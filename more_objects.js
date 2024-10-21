4a3abdd4919432af478e5e9fdcab4fd93e66203b
aloro lee <alorotams@gmail.com>
2024-10-21 10:31:12 -0400

    more objects

---
+++ more_objects.js
@@ -0,0 +1,24 @@
+const person = { //key and values (create an object)
+    first: "lee",
+    last: "Aloro",
+    age: 30,
+    phone: "344-333-3333",
+    DOB: new Date("9-53-1324")
+};
+
+person.hobby = "pool"; //adding to the object list
+
+console.log(person.DOB)
+console.log(person.age)
+console.log(person.hobby)
+
+const fruit = [ //no key (create arrays)
+    "apple",
+    "banana",
+    "cherry"
+]
+console.log(fruit[1])
+
+fruit.push("pineapple") //.push add to the end of the list
+
+console.log(fruit[3])
