45840624c5220cb976736e4176e2ad9eb6eff146
aloro lee <alorotams@gmail.com>
2024-10-22 15:36:17 -0400

    board

---
+++ board.js
@@ -0,0 +1,18 @@
+// columns and rows
+// smaller than go game
+// chess checkers 8x8
+// battleship 10x10
+// globe let lng 180x180
+// tic tac toe nought an1 crosses 3x3
+
+const line = [".",".","."]
+line[1] = "x";
+console.log(line);
+
+const board = [
+    ["x",".","."],
+    [".",".","."],
+    [".",".","x"],
+];
+
+board[1][1]
