a405c273052ba1087e00860884b267370ad25901
aloro lee <alorotams@gmail.com>
2024-10-22 13:38:04 -0400

    objects

--- average.js
+++ average.js
@@ -1,24 +1,37 @@
 
 let sumTotal = 0;
 
-function addThemUp(numberList){
+// function addThemUp(numberList){
+//     let sumTotal = 0;
+//     for (let i = 0; i < numbers.length; i++){
+//         let n = numberList[i];
+//         sumTotal += n;
+//         // sumTotal = sumTotal + n
+//     }
+//     return sumTotal;
+// }
+
+// let numbers = [6, 3, 5, 0, 2];
+// let total = addThemUp(numbers)
+// console.log(total);
+
+// let quantity = numbers.length;
+// let average = total / quantity
+// console.log("AVERAGE:", average)
+
+// short version
+
+let numbers = [6, 3, 5, 0, 2];
+
+function getAverage(numberList){
     let sumTotal = 0;
-    for (let i = 0; i < numbers.length; i++){
+    for (let i = 0; i < numberList.length; i++){
         let n = numberList[i];
         sumTotal += n;
         // sumTotal = sumTotal + n
     }
-    return sumTotal;
+    return sumTotal / numberList.length;
 }
-
-let numbers = [6, 3, 5, 0, 2];
-let total = addThemUp(numbers)
-console.log(total);
-
-let quantity = numbers.length;
-let average = total / quantity
-console.log("AVERAGE:", average)
-
-
+console.log("AVG:",getAverage(numbers))
 
 
---
+++ objects_vs_lists.js
@@ -0,0 +1,51 @@
+// OBJECT {} VS ARRAYS []
+
+// fight!
+
+// access by index vs access by key (string or number)
+
+// array is accessed by index AKA position
+// objects values are access by key 
+
+// All arrays are objects
+// Most objects are not arrays 
+
+// All dogs are mammals,
+// not all mammals are dogd..
+
+Object = {
+    property1: "value1",
+    property2: "value2",
+    property3: "value3",
+}
+
+// Objects are NOUNS 
+car = {
+    price: 9999,
+    make: "FORD",
+    model: "MUSTANG",
+    engine: {
+        engineType: "Inline 4",
+        pistons: [{rodThrown: false},{rodThrown: false},{rodThrown: false},{rodThrown: false},]
+
+    }
+}
+
+htmlElemnt = {
+    tegName: "div",
+    class: ["hidden", "large", "col-3"],
+    id: "thingy",
+    value: "123"
+}
+
+list = [] //new array
+list2 = new Array(); // new Object
+
+colors = ["red","green","blue"]
+console.log(colors[1]); //short hand
+console.log(colors.at(0))
+now = new Date(); // date object
+count = new Number("123"); //number object
+
+thing1 = {}; //object literal
+thing2 = new Object();
