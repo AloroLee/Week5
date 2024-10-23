2ca9cf1a1fb300b538abc7b7ba5b1fb9b29a8b74
aloro lee <alorotams@gmail.com>
2024-10-23 08:21:32 -0400

    updates

--- average.js
+++ average.js
@@ -34,4 +34,27 @@ function getAverage(numberList){
 }
 console.log("AVG:",getAverage(numbers))
 
+let studentList1 = [  // array of students
+    { name: "Zephaniah", scores: [100, 96, 99, 92] },
+    { name: "Pursalane", scores: [92, 89, 96, 100, 94] },
+    { name: "Siddalee", scores: [86, 72, 92] }
+];
+
+let studentList2 = [
+    { name: "Ian", scores: [98, 84, 89, 100, 100, 76] },
+    { name: "Elisha", scores: [89, 100] },
+    { name: "Ezra", scores: [100, 99, 100, 87] }
+];
+
+function showAverages(studentList){
+    for (let i = 0; i < studentList.length; i++) {
+        const student = studentList[i];
+        const average = getAverage(student.scores);
+        const textAverage = average.toFixed(1);
+        console.log(student.name, textAverage);
+    }
+}
+showAverages(studentList1);
+showAverages(studentList2);
+
 
